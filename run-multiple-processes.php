24bbf308a9c0b5c5ac4787e0bf3e8863e2f043f5
Jan Mikeš <j.mikes@me.com>
2020-04-15 19:26:36 +0200

    Measure speedup

--- run-multiple-processes.php
+++ run-multiple-processes.php
@@ -5,21 +5,25 @@ require_once __DIR__ . '/vendor/autoload.php';
 $results = [];
 
 \Amp\Loop::run(function() use (&$results) {
-	$callback = function(\Amp\Process\Process $process) use (&$results): \Generator {
-		yield $process->start();
-
-		$json = yield \Amp\ByteStream\buffer($process->getStdout());
-
-		return json_decode($json);
-	};
-
 	$promises = [];
+
 	for($i=1 ; $i<=10 ; $i++) {
 		$process = new \Amp\Process\Process(['php', 'script.php']);
-		$promises[] = new \Amp\Coroutine($callback($process));
+
+		$promises[] = \Amp\call(function() use ($process): \Generator {
+			yield $process->start();
+
+			$json = yield \Amp\ByteStream\buffer($process->getStdout());
+
+			return json_decode($json);
+		});
 	}
 
 	$results = yield \Amp\Promise\all($promises);
 });
 
-print_r($results);
+$time = microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'];
+
+echo "Script took ${time} seconds to run\n";
+
+// print_r($results);
